547462159b6632cfbd56bb1d32c17dad20307509
Marcus <marcus.ekman@kungsbacka.se>
2020-02-27 12:08:30 +0100

    Second sout

--- src/pkg1/firstprogram/FirstProgram.java
+++ src/pkg1/firstprogram/FirstProgram.java
@@ -5,6 +5,8 @@ public class FirstProgram {
 
     public static void main(String[] args) {
             System.out.println("Hello World!");
+            
+            System.out.println("Just another hello for the world!");
     }
     
 }
